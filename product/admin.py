841618a248ce98b6a18f5bd91f22eedb32f84774
Pinky <pinkymononyane@gmail.com>
2022-07-10 11:38:38 +0200

    add product models

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from product.models import *
+
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'parent', 'status']
+    list_filter = ['status']
+    
+class ProductImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'status', 'image_tag']
+    list_filter = ['category']
+    readonly_fields = ('image_tag',)
+    inlines = [ProductImageInline]
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Images)
--- product/models.py
+++ product/models.py
@@ -1,4 +1,6 @@
+from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
+from django.utils.safestring import mark_safe
 # from mptt.fields import TreeForeignKey
 # from mptt.models import MPTTModel
 
@@ -11,7 +13,7 @@ class Category(models.Model):
         ('False', 'False'),
     )
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
     keyword = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -22,4 +24,55 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Product(models.Model):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    AVAILABILITY = (
+        ('In Stock', 'In Stock'),
+        ('Out Of Stock', 'Out Of Stock'),
+    )
+    CONDITION = (
+        ('New', 'New'),
+        ('Hot', 'Hot'),
+        ('Sale', 'Sale'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    keyword = models.CharField(max_length=30)
+    description = RichTextUploadingField(blank=True, null=True)
+    information = RichTextUploadingField(blank=True, null=True)
+    small_product_details = RichTextUploadingField(blank=True, null=True)
+    slug = models.SlugField(null=False, unique=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+    price = models.FloatField(null=True, blank=True)
+    amount = models.FloatField(null=True, blank=True)
+    minamount = models.FloatField(null=True, blank=True)
+    status = models.CharField(max_length=10, choices=STATUS)
+    availability = models.CharField(max_length=15, choices=AVAILABILITY)
+    condition = models.CharField(max_length=10, choices=CONDITION)
 
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+    def image_tag(self):
+            if self.image_tag:
+                return mark_safe('<img src="{}" height="50" />'.format(self.image.url))
+            return ''
+
+    image_tag.short_description = 'Image'
+    
+
+class Images(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+
+    def __str__(self):
+        return self.title
